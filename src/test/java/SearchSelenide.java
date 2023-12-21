0765d5046a3a7b642c7a5b16f0375b8b7d9a05d1
Sokhorov Basang <basanya91@yandex.ru>
2023-12-21 13:54:12 +0300

    add test SearchSelenide

---
+++ src/test/java/SearchSelenide.java
@@ -0,0 +1,43 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchSelenide {
+    @BeforeAll
+    static void beforeAll(){
+//        Configuration.browserSize = "1920x1080";
+//        Configuration.browser = "firefox";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+
+    }
+
+
+    @Test
+    void searchSelenide (){
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("[href=\"/selenide/selenide/wiki/SoftAssertions\"]").click();
+
+        $(".gh-header-title").shouldHave(text("SoftAssertions"));
+
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+
+    }
+}
